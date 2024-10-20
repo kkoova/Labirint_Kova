5f4d93ea838ac9c5c98aa3fe08ab18204b515000
kova <alya.korobeynikova99@gmail.com>
2024-10-20 20:38:43 +0300

    MainForm

--- Labirint_Kova/Forms/MainForm.cs
+++ Labirint_Kova/Forms/MainForm.cs
@@ -12,9 +12,64 @@ namespace Labirint_Kova
 {
     public partial class MainForm : Form
     {
+        private float angle = 0f;
         public MainForm()
         {
             InitializeComponent();
+            TimerIn();
+        }
+
+        private void TimerIn()
+        {
+            rotationTimer.Interval = 150;
+            rotationTimer.Tick += RotationTimer_Tick;
+            rotationTimer.Start();
+        }
+        private void ExitBtn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void Button_MouseEnter(object sender, EventArgs e)
+        {
+            if (sender is Label button)
+            {
+                button.ForeColor = Color.DarkOliveGreen;
+            }
+        }
+
+        private void Button_MouseLeave(object sender, EventArgs e)
+        {
+            if (sender is Label button)
+            {
+                button.ForeColor = Color.Black;
+            }
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RotationTimer_Tick(object sender, EventArgs e)
+        {
+            angle += 1f;
+            if (angle >= 360f)
+            {
+                angle = 0f;
+            }
+            MazePngOic.Invalidate();
+        }
+
+        private void MazePngOic_Paint(object sender, PaintEventArgs e)
+        {
+            Image img = Properties.Resources.MazePng;
+
+            e.Graphics.TranslateTransform((float)MazePngOic.Width / 2, (float)MazePngOic.Height / 2);
+            e.Graphics.RotateTransform(angle);
+            e.Graphics.TranslateTransform(-(float)MazePngOic.Width / 2, -(float)MazePngOic.Height / 2);
+
+            e.Graphics.DrawImage(img, new Point(0, 0));
         }
     }
 }
