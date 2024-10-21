fa5b33ebdf4d93e73e43e6afc79c81ad64f2d105
Korobeynikova <Korobeynikova@B329-L01>
2024-10-21 12:04:45 +0300

    blockAdd

---
+++ Labirint_Kova/Logic/GenerateMaze.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labirint_Kova.Logic
+{
+    internal class GenerateMaze
+    {
+        static readonly int[,] maze = new int[41, 21];
+
+        /// <summary>
+        /// Генерация лабиринта
+        /// </summary>
+        public void GenerateNumerMaze()
+        {
+            for (var y = 0; y < maze.GetLength(0); y++)
+            {
+                for (var x = 0; x < maze.GetLength(1); x++)
+                {
+                    maze[x, y] = 1;
+                }
+            }
+
+            var rand = new Random();
+            var stack = new Stack<(int x, int y)>();
+            int startX = 1, startY = 1;
+            maze[startX, startY] = 0;
+            stack.Push((startX, startY));
+
+            (int x, int y)[] directions = { (0, -2), (0, 2), (-2, 0), (2, 0) };
+
+            while (stack.Count > 0)
+            {
+                var (currentX, currentY) = stack.Peek();
+                var neighbors = new List<(int x, int y)>();
+
+                foreach (var (dx, dy) in directions)
+                {
+                    var nx = currentX + dx;
+                    var ny = currentY + dy;
+
+                    if (nx > 0 && nx < maze.GetLength(0) - 1 && ny > 0 && ny < maze.GetLength(1) - 1 && maze[nx, ny] == 1)
+                    {
+                        neighbors.Add((nx, ny));
+                    }
+                }
+
+                if (neighbors.Count > 0)
+                {
+                    var (nextX, nextY) = neighbors[rand.Next(neighbors.Count)];
+                    maze[(currentX + nextX) / 2, (currentY + nextY) / 2] = 0;
+                    maze[nextX, nextY] = 0;
+                    stack.Push((nextX, nextY));
+                }
+                else
+                {
+                    stack.Pop();
+                }
+            }
+        }
+    }
+}
